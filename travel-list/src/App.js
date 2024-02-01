a4d1aac624fd4776edf62ef514095cfba805cd47
Muhammad Zayem <mzayemazam@gmail.com>
2024-02-01 14:05:15 +0500

    list redering

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "charger", quantity: 1, packed: true },
+  { id: 4, description: "keys", quantity: 1, packed: false },
+];
 export default function App() {
   return (
     <div className="app">
@@ -21,7 +27,25 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">list</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
   return (
