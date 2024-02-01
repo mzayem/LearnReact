d29acc16dbdd6ffd0b48b5ab99a56090b99d7ffc
Muhammad Zayem <mzayemazam@gmail.com>
2024-02-01 13:02:42 +0500

    layout breakdowns and classes of CSS settings

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,11 +1,32 @@
-import "./App.css";
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <p>iekrjs</p>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for trip? 😍</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">list</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼You have X item in your list, you have already packed X(X%)</em>
+    </footer>
+  );
+}
